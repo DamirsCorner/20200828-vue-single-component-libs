68418b7ebef5e2a001a37b127cc6494586aa6e98
Damir Arh <damir.arh@gmail.com>
2020-07-30 21:18:20 +0200

    Initial commit
    Imported from https://github.com/DamirsCorner/20200821-vue-component-lib

---
+++ vue-component/src/components/Button.vue
@@ -0,0 +1,24 @@
+<template>
+  <button class="button" type="button" @click="click()">
+    <slot>{{ text }}</slot>
+  </button>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Button extends Vue {
+  @Prop({ type: String, required: true }) readonly text!: string;
+
+  click() {
+    this.$emit("click");
+  }
+}
+</script>
+
+<style scoped>
+.button {
+  border-color: black;
+}
+</style>
